f180565d36d60c12ad838fc25c7ed8a8b03221e3
lucielany <luciela@hotmail.es>
2023-06-20 12:43:10 +0200

    web testimonios

---
+++ componente-testimonio/src/App.jsx
@@ -0,0 +1,42 @@
+import './App.css';
+import Testimonio from './componentes/Testimonio';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="contenedor-principal">
+        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+        <Testimonio 
+          nombre="Shawn Wang"
+          pais="Singapur"
+          imagen="testimonio-shawn.png"
+          nombreFoto="Foto de shawn"
+          cargo="Ingeniero de Software"
+          empresa="Amazon"
+          testimonio="Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. <b>freeCodeCamp cambió mi vida.</b>"
+        />
+        <Testimonio 
+          nombre="Sarah Chima"
+          pais="Nigeria"
+          imagen="testimonio-sarah.png"
+          nombreFoto="Foto de sarah"
+          cargo="Ingeniera de Software"
+          empresa="ChatDesk"
+          testimonio="<b>freeCodeCamp fue la puerta de entrada a mi carrera</b> como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble."
+        />
+        <Testimonio 
+          nombre="Emma Bostian"
+          pais="Suecia"
+          imagen="testimonio-emma.png"
+          nombreFoto="Foto de emma"
+          cargo="Ingeniera de Software"
+          empresa="Spotify"
+          testimonio="Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en <b>freeCodeCamp me dio las habilidades</b> y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify"
+        />
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
